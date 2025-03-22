20213ba7706a84043311a167e33a311dc907fa8a
Aditya Joshi <joshiaditya.aj07@gmail.com>
2025-03-22 21:46:16 +0530

    feat: add Google Analytics tracking scripts to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script"; // Import Script component
 import Navbar from "./components/Navbar";
 import "./globals.css";
 
@@ -17,6 +18,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full">
+      <head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}');
+          `}
+        </Script>
+      </head>
       <body
         className={`bg-[#161716] ${inter.className} antialiased min-h-full`}
       >
