4f54e43a06ce2b54347db624752b269df7b932f8
Aditya Joshi <103844076+adityaj07@users.noreply.github.com>
2025-03-23 14:04:24 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+-0import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Script from "next/script"; // Import Script component
 import Navbar from "./components/Navbar";
@@ -28,7 +28,7 @@ export default function RootLayout({
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}');
+            gtag('config', ${process.env.GOOGLE_ANALYTICS_ID});
           `}
         </Script>
       </head>
