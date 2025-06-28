d840b295b58b646e512f09e9cd83550bce6209c0
Aditya Joshi <103844076+adityaj07@users.noreply.github.com>
2025-06-28 20:28:12 +0530

    Update data.ts

--- data.ts
+++ data.ts
@@ -193,7 +193,7 @@ export const projects = [
     slug: "uplog",
     desc: "Uplog is the modern changelog manager designed for SaaS founders and indie devs. Capture every feature release, bug fix, and improvement—publish them on a beautifully designed public page.",
     longDescription: "",
-    liveLink: "https://uplog.adityaj07.tech",
+    liveLink: "https://uplog.vercel.app",
     githubLink: "https://github.com/adityaj07/uplog",
     coverImg: "/projects/project-uplog.jpg",
     imgs: [],
